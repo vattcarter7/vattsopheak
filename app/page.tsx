f53d1fc092c267c8167ecfddccc67fec733297c5
Vatt Sopheak <vattcarter@gmail.com>
2023-12-10 13:27:24 +0700

    Changed favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Vatt Sopheak',
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
